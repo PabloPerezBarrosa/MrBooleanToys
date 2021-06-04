2eb9e5012e2522c8598e9f53cd200ed226249f14
PabloPerezBarrosa <pperezbarrosa@gmail.com>
2021-06-04 17:51:49 +0200

    Más

--- src/main/java/com/mrboolean/controller/CarritoController.java
+++ src/main/java/com/mrboolean/controller/CarritoController.java
@@ -22,7 +22,7 @@ public class CarritoController implements Serializable {
 
     @PostConstruct
     public void init() {
-        
+
         producto = new Producto();
 
     }
@@ -34,45 +34,48 @@ public class CarritoController implements Serializable {
     public void añadirCartItem() {
         List<CartItem> itemList = new ArrayList<CartItem>();
         try {
-           
-            boolean match = false;
-            itemList = (List<CartItem>) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("carrito");
-            CartItem item = new CartItem();
-            
-            item.setProducto(this.producto);
-            item.setCantidad(this.cantidad);
 
-            if (itemList != null && !itemList.isEmpty()) {
-                System.out.println("NO SOY NULL" + itemList.size());
-                for (CartItem ci : itemList) {
+            if (this.producto.getStock() >= this.cantidad) {
 
-                    if (ci.getProducto().getIdproducto() == item.getProducto().getIdproducto()) {
+                boolean match = false;
+                itemList = (List<CartItem>) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("carrito");
+                CartItem item = new CartItem();
 
-                        ci.setCantidad(ci.getCantidad() + item.getCantidad());
-                        match = true;
-                        break;
+                item.setProducto(this.producto);
+                item.setCantidad(this.cantidad);
+
+                if (itemList != null && !itemList.isEmpty()) {
+
+                    for (CartItem ci : itemList) {
+
+                        if (ci.getProducto().getIdproducto() == item.getProducto().getIdproducto()) {
+
+                            ci.setCantidad(ci.getCantidad() + item.getCantidad());
+                            match = true;
+                            break;
+                        }
                     }
-                }
-                if (!match) {
+                    if (!match) {
+                        itemList.add(item);
+                    }
+                } else {
+        
                     itemList.add(item);
                 }
-            } else {
-                System.out.println("SOY NULL");
-                itemList.add(item);
+
+                FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("carrito", itemList);
+
+                System.out.println("SIGGUIENTE VUELTA");
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Correcto", "Articulo añadido al carrito"));
+                PrimeFaces.current().executeScript("PF('wcart').hide();");
+                
+            }else{
+                
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Error", "No hay unidades en stock suficientes, quedan: "+ this.producto.getStock()));
+                PrimeFaces.current().executeScript("PF('wcart').hide();");
+                
             }
 
-            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("carrito", itemList);
-
-        for(CartItem i : itemList){
-            
-        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXX");
-        System.out.println(i.getProducto().getNombre() + "cantidad: " + i.getCantidad());
-        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXX");
-            
-        }
-        System.out.println("SIGGUIENTE VUELTA");
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Correcto", "Articulo añadido al carrito"));
-            PrimeFaces.current().executeScript("PF('wcart').hide();");
         } catch (Exception e) {
 
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Error", "Fallo añadiendo al carrito"));
