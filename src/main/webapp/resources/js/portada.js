2a4536970f8411ffbd530e121ab3ccf7d0f201ff
PabloPerezBarrosa <pperezbarrosa@gmail.com>
2021-06-08 16:54:31 +0200

    Antes de meter imagen usuario

--- src/main/webapp/resources/js/portada.js
+++ src/main/webapp/resources/js/portada.js
@@ -2,3 +2,6 @@
 function redirect() {
   window.setTimeout(function(){location.href="faces/principal.xhtml"}, 2000);
 }
+function redirect2() {
+  window.setTimeout(function(){location.href="/MrBooleanToys/faces/principal.xhtml"}, 2000);
+}
