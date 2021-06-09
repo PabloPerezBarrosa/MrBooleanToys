1dac90085efe256d1f844007c2176bf0ba13b4b0
PabloPerezBarrosa <pperezbarrosa@gmail.com>
2021-06-09 10:28:57 +0200

    Backup antes de meter DataGrid

--- src/main/java/com/mrboolean/controller/MenuController.java
+++ src/main/java/com/mrboolean/controller/MenuController.java
@@ -20,13 +20,14 @@ import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
 import javax.servlet.http.HttpServletRequest;
 import org.primefaces.PrimeFaces;
 import org.primefaces.context.RequestContext;
 import org.primefaces.model.UploadedFile;
 
 @Named(value = "menuController")
-@SessionScoped
+@ViewScoped
 public class MenuController implements Serializable {
 
     @EJB
